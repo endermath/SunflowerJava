3e5750d04b8bbaab66d828aba8c9a62cdea5e4a7
Endermath <jonas.hartwig@gmail.com>
2011-12-28 17:45:36 -0800

    test

--- src/SunflowerApplet.java
+++ src/SunflowerApplet.java
@@ -4,7 +4,7 @@ import java.applet.*;
 public class SunflowerApplet extends Applet {
 	public void paint(Graphics g) {
 		g.drawString("Sandy's Sunflowers",20,30);
-		g.drawString("Arrow keys to move",30,150);
+		g.drawString("Arrow keys to move",30,50);
 		g.drawString("Space to water", 20, 70); 
 		g.drawString("Space to water and Return to plant", 20, 90);
 		
